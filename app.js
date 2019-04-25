e6fe805f229045dd200ad993ea18d665ec6467a8
Lindsey Duquette <duquette.lr@gmail.com>
2019-04-24 21:40:33 -0400

    fixed buttons

--- app.js
+++ app.js
@@ -3,26 +3,41 @@
 //append button element to screen in first container
 
 
+
+//questions: why is my push function overwriting my array?
+var index = 0;
+var movies = ["Back to the future", "Sixteen Candles", "Dirty Dancing", "Ghostbusters", "The Breakfast Club"];
+
 $("#addButton").on("click", function (e) {
+
+
 
     e.preventDefault();
     var movieInput = $("#addMovieInput").val().trim();
     console.log(movieInput);
 
 
+    // $(".buttonDisplay").empty();
+
     if (movieInput !== "") {
-        $(".buttonDisplay").append(`<button class="movieButton">${movieInput}</button>`);
+        $(".buttonDisplay").append(`<button class="movieButton${index}">${movieInput}</button>`);
     }
 
 
 
-    $(".movieButton").on("click", function (e) {
+    //document on click
 
-        var movieGifSearch = $(".movieButton").text();
+
+    $(`.movieButton${index}`).on("click", function (e) {
+
+        var movieGifSearch = $(this).text();
+
+
         console.log(movieGifSearch);
         var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=a6Zna39740OrKHn6vHZDCM4dYZCnukIh&q=${movieGifSearch}&limit=25&offset=0&rating=G&lang=en`;
 
-        // e.preventDefault();
+        e.preventDefault();
+
 
         $.ajax({
             url: queryURL,
@@ -30,16 +45,26 @@ $("#addButton").on("click", function (e) {
         }).then(function (response) {
             console.log(response);
 
-            var movieGif = response.data[0].images.original_still.url;
-            console.log(movieGif);
+            var movieGif = response.data[0].images.original.url;
+
+            var movieGifStill = response.data[0].images.original_still.url;
 
             $(".giphDisplay").prepend(`<img src=${movieGif}>`);
+
+            //add still and animate states (original url vs original_still url)
+            //push your created buttons into an array
+            //figure out event delegation???
 
 
         });
 
 
+
     });
+
+    index++;
+
+
 
 });
 
